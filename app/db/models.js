c68cc439c85e2a1d8062dc66e34fa9081d762b47
Frederik Graakjær <graakjaer.f@gmail.com>
2024-03-06 09:41:46 +0100

    schemas and seeding

--- app/db/models.js
+++ app/db/models.js
@@ -1,35 +1,140 @@
-import { mongoose } from "mongoose";
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
-const { Schema } = mongoose;
-
-const entrySchema = new Schema(
+// ========== models ========== //
+const userSchema = new mongoose.Schema(
   {
-    date: {
-      type: Date,
-      required: true,
-    },
-    type: {
+    image: String,
+    mail: {
       type: String,
-      enum: ["work", "learning", "interesting-thing"],
-      required: true,
+      required: true, // Ensure user emails are required
+      unique: true, // Ensure user emails are unique
     },
-    text: {
+    name: String,
+    password: {
       type: String,
-      required: true,
+      required: true, // Ensure user passwords are required
+      select: false, // Automatically exclude from query results
     },
   },
-  // Automatically add `createdAt` and `updatedAt` timestamps:
-  // https://mongoosejs.com/docs/timestamps.html
   { timestamps: true },
 );
+// pre save password hook
+userSchema.pre("save", async function (next) {
+  const user = this; // this refers to the user document
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified("password")) {
+    return next(); // continue
+  }
+
+  const salt = await bcrypt.genSalt(10); // generate a salt
+  user.password = await bcrypt.hash(user.password, salt); // hash the password
+  next(); // continue
+});
+
+// const entrySchema = new mongoose.Schema(
+//   {
+//     date: {
+//       type: Date,
+//       required: true,
+//     },
+//     type: {
+//       type: String,
+//       enum: ["work", "learning", "interesting-thing"],
+//       required: true,
+//     },
+//     text: {
+//       type: String,
+//       required: true,
+//     },
+//   },
+//   // Automatically add `createdAt` and `updatedAt` timestamps:
+//   // https://mongoosejs.com/docs/timestamps.html
+//   { timestamps: true },
+// );
 
 // For each model you want to create, please define the model's name, the
 // associated schema (defined above), and the name of the associated collection
 // in the database (which will be created automatically).
-export const models = [
+const eventSchema = new mongoose.Schema(
   {
-    name: "Entry",
-    schema: entrySchema,
-    collection: "entries",
+    title: {
+      type: String,
+      required: true,
+      minLength: [3, "Too short, min is 3 characters"],
+      maxLength: [60, "Too long, max is 60 characters"],
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: new mongoose.Types.ObjectId("65cde4cb0d09cb615a23db17"),
+    },
+    likes: Number,
+    tags: [String],
   },
+  { timestamps: true },
+);
+export const models = [
+  // {
+  //   name: "Entry",
+  //   schema: entrySchema,
+  //   collection: "entries",
+  // },
+  { name: "User", schema: userSchema, collection: "users" },
+  { name: "Event", schema: eventSchema, collection: "events" },
 ];
+
+// async function insertData() {
+//   const User = mongoose.models.User;
+
+//   await User.collection.drop();
+
+//   // const entries = [
+//   //   {
+//   //     date: new Date("2024-01-01"),
+//   //     type: "work",
+//   //     text: "I'm working",
+//   //   },
+//   //   {
+//   //     date: new Date("2024-01-15"),
+//   //     type: "learning",
+//   //     text: "I'm learning",
+//   //   },
+//   //   {
+//   //     date: new Date("2024-02-01"),
+//   //     type: "interesting-thing",
+//   //     text: "I'm doing something interesting",
+//   //   },
+//   //   {
+//   //     date: new Date("2024-02-15"),
+//   //     type: "learning",
+//   //     text: "Remix Auth with FormStrategy and Post App",
+//   //   },
+//   //   {
+//   //     date: new Date("2024-02-22"),
+//   //     type: "work",
+//   //     text: "Remix Work Journal",
+//   //   },
+//   // ];
+//   const users = [
+//     {
+//       email: "test@123.com",
+//       password: "123",
+//       name: "test",
+//     },
+//     //one more user
+//     {
+//       email: "test@1234.com",
+//       password: "1234",
+//       name: "test2",
+//     },
+//   ];
+
+//   // await mongoose.models.Entry.insertMany(entries);
+//   await mongoose.models.User.insertMany(users);
+// }
--- app/db/seedingDb.server.js
+++ app/db/seedingDb.server.js
@@ -1,40 +1,72 @@
 import mongoose from "mongoose";
-
 export default async function seedDb() {
-  const entryCount = await mongoose.models.Entry.countDocuments();
-  if (entryCount === 0) {
-    console.log("Seeding database...");
-    insertData();
+  // check if data exists
+  const userCount = await mongoose.models.User.countDocuments();
+  const eventCount = await mongoose.models.Event.countDocuments();
+
+  if (userCount === 0 || eventCount === 0) {
+    await insertData();
   }
 }
 
 async function insertData() {
-  const entries = [
+  const User = mongoose.models.User;
+  const Event = mongoose.models.Event;
+
+  console.log("Dropping collections...");
+  await User.collection.drop();
+  await Event.collection.drop();
+
+  console.log("Inserting data...");
+  // Insert users
+  const maria = await User.create({
+    image:
+      "https://www.baaa.dk/media/b5ahrlra/maria-louise-bendixen.jpg?anchor=center&mode=crop&width=800&height=450&rnd=132792921650330000&format=webp",
+    mail: "mlbe@eaaa.dk",
+    name: "Maria Louise Bendixen",
+    password: "maria1234",
+  });
+
+  const rasmus = await User.create({
+    _id: new mongoose.Types.ObjectId("65cde4cb0d09cb615a23db17"),
+    image: "https://share.cederdorff.dk/images/race.webp",
+    mail: "race@eaaa.dk",
+    name: "Rasmus Cederdorff",
+
+    password: "rasmus1234",
+  });
+
+  const dan = await User.create({
+    image:
+      "https://www.eaaa.dk/media/bdojel41/dan-okkels-brendstrup.jpg?anchor=center&mode=crop&width=800&height=450&rnd=132792921559630000&format=webp",
+    mail: "dob@eaaa.dk",
+    name: "Dan Okkels Brendstrup",
+    password: "1234",
+  });
+
+  // Insert posts
+  await Event.insertMany([
     {
-      date: new Date("2024-01-01"),
-      type: "work",
-      text: "I'm working",
+      title: "Beautiful sunset at the beach",
+      image:
+        "https://images.unsplash.com/photo-1566241832378-917a0f30db2c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=80",
+      user: maria._id,
+      tags: ["beach", "sunset", "nature", "Aarhus"],
     },
     {
-      date: new Date("2024-01-15"),
-      type: "learning",
-      text: "I'm learning",
+      title: "Exploring the city streets of Aarhus",
+      image:
+        "https://images.unsplash.com/photo-1559070169-a3077159ee16?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1pYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=80",
+      user: dan._id,
+      tags: ["city", "Aarhus", "exploration"],
     },
+
     {
-      date: new Date("2024-02-01"),
-      type: "interesting-thing",
-      text: "I'm doing something interesting",
+      title: "Exploring the city center of Aarhus",
+      image:
+        "https://images.unsplash.com/photo-1612624629424-ddde915d3dc5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1pYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=80",
+      user: rasmus._id,
+      tags: ["city", "Aarhus", "exploration", "cityhall"],
     },
-    {
-      date: new Date("2024-02-15"),
-      type: "learning",
-      text: "Remix Auth with FormStrategy and Post App",
-    },
-    {
-      date: new Date("2024-02-22"),
-      type: "work",
-      text: "Remix Work Journal",
-    },
-  ];
-  await mongoose.models.Entry.insertMany(entries);
+  ]);
 }
